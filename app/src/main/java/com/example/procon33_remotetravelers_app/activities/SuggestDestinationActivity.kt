29b8a688ca39ba0dcd76efbc5a5d4f0aeb0588a2
Mitsuki4123 <mitsukikosen7@gmail.com>
2022-08-22 16:22:56 +0900

    SuggestDestinationActivityを追加

---
+++ app/src/main/java/com/example/procon33_remotetravelers_app/activities/SuggestDestinationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.procon33_remotetravelers_app.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.procon33_remotetravelers_app.R
+
+class SuggestDestinationActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_viewer)
+    }
+}
