7fea0b1852be4304be518a31e51dd1c64d4083b0
KubotaShunsuke <s.kubota0921@gmail.com>
2022-08-27 23:44:17 +0900

    現在地の取得に成功 #41

--- app/src/main/java/com/example/procon33_remotetravelers_app/activities/TravelerActivity.kt
+++ app/src/main/java/com/example/procon33_remotetravelers_app/activities/TravelerActivity.kt
@@ -1,13 +1,27 @@
 package com.example.procon33_remotetravelers_app.activities
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.location.LocationManager.GPS_PROVIDER
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.example.procon33_remotetravelers_app.R
 import com.example.procon33_remotetravelers_app.databinding.ActivityTravelerBinding
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -16,10 +30,10 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import java.util.*
+import kotlin.properties.Delegates
 
 
-class TravelerActivity : AppCompatActivity(), OnMapReadyCallback {
+class TravelerActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     companion object {
         const val CAMERA_REQUEST_CODE = 1
@@ -28,6 +42,22 @@ class TravelerActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityTravelerBinding
+    private lateinit var locationManager: LocationManager
+    private var latitude by Delegates.notNull<Double>()
+    private var longitude by Delegates.notNull<Double>()
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // 使用が許可された
+            locationStart()
+        } else {
+            // それでも拒否された時の対応
+            val toast = Toast.makeText(this,
+                "位置情報の利用を許可してください", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,6 +75,67 @@ class TravelerActivity : AppCompatActivity(), OnMapReadyCallback {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             resultLauncher.launch(intent)
         }
+
+        val testButton = findViewById<Button>(R.id.test_button)
+        testButton.setOnClickListener {
+            if(::mMap.isInitialized){
+                var me = LatLng(latitude, longitude)
+                mMap.addMarker(MarkerOptions().position(me).title("me"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(me))
+            }
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        } else {
+            locationStart()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun locationStart() {
+        Log.d("debug", "locationStart()")
+
+        // Instances of LocationManager class must be obtained using Context.getSystemService(Class)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        if (locationManager.isProviderEnabled(GPS_PROVIDER)) {
+            Log.d("debug", "location manager Enabled")
+        } else {
+            // to prompt setting up GPS
+            val settingsIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(settingsIntent)
+            Log.d("debug", "not gpsEnable, startActivity")
+        }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1000)
+
+            Log.d("debug", "checkSelfPermission false")
+        } else
+            locationManager.requestLocationUpdates(
+            GPS_PROVIDER,
+            1000,
+            50f,
+            this)
+    }
+
+    override fun onLocationChanged(location: Location) {
+        latitude = location.latitude
+        longitude = location.longitude
+    }
+
+    override fun onProviderEnabled(provider: String) {
+
+    }
+
+    override fun onProviderDisabled(provider: String) {
+
     }
 
     /**
@@ -59,10 +150,9 @@ class TravelerActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        var me = LatLng(latitude, longitude)
+        mMap.addMarker(MarkerOptions().position(me).title("me"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(me))
     }
 
     var resultLauncher = registerForActivityResult(
