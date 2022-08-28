958b293c5154dd830d5b7895105ede8f1ba3c3a1
Mitsuki4123 <mitsukikosen7@gmail.com>
2022-08-28 15:57:41 +0900

    画像表示_仮完成

--- app/src/main/java/com/example/procon33_remotetravelers_app/activities/CreateReportActivity.kt
+++ app/src/main/java/com/example/procon33_remotetravelers_app/activities/CreateReportActivity.kt
@@ -1,12 +1,20 @@
 package com.example.procon33_remotetravelers_app.activities
 
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.procon33_remotetravelers_app.R
 
 class CreateReportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_report)
+
+        val intent = intent
+        val photo = intent.getParcelableExtra<Bitmap>("data")
+
+        val imageView = findViewById<ImageView>(R.id.report_image)
+        imageView.setImageBitmap(photo)
     }
 }
