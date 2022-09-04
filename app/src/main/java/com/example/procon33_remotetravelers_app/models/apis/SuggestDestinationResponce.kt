5ebcd227f1e38df54c48c24648dd8434de8306f2
KubotaShunsuke <s.kubota0921@gmail.com>
2022-09-04 22:02:03 +0900

    ピン情報を保存するAPIをたたく処理、ピン設置をキャンセルする機能を実装 #36

---
+++ app/src/main/java/com/example/procon33_remotetravelers_app/models/apis/SuggestDestinationResponce.kt
@@ -0,0 +1,6 @@
+package com.example.procon33_remotetravelers_app.models.apis
+
+data class SuggestDestinationResponse(
+    val error: String?,
+    val ok: Boolean,
+)
---
+++ app/src/main/java/com/example/procon33_remotetravelers_app/services/SuggestDestinationService.kt
@@ -0,0 +1,18 @@
+package com.example.procon33_remotetravelers_app.services
+
+import com.example.procon33_remotetravelers_app.models.apis.SuggestDestinationResponse
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+sealed interface SuggestDestinationService {
+    @FormUrlEncoded
+    @POST("/api/common/save-location")
+    fun suggestDestination(
+        @Field("user_id") user_id: Int,
+        @Field("lat") lat: Double,
+        @Field("lon") lon: Double,
+        @Field("suggestion_flag") suggestion_flag: Int,
+    ): Call<SuggestDestinationResponse>
+}
