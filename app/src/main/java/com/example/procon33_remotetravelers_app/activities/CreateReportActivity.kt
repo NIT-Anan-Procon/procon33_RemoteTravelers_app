3fbcef3b2d5e6eb2e8cc5d67f41621583e43141e
Mitsuki4123 <mitsukikosen7@gmail.com>
2022-09-05 14:38:15 +0900

    activityの準備

--- app/src/main/java/com/example/procon33_remotetravelers_app/activities/CreateReportActivity.kt
+++ app/src/main/java/com/example/procon33_remotetravelers_app/activities/CreateReportActivity.kt
@@ -5,9 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import com.example.procon33_remotetravelers_app.BuildConfig
 import com.example.procon33_remotetravelers_app.R
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class CreateReportActivity : AppCompatActivity() {
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private  val retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.API_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_report)
@@ -24,11 +38,17 @@ class CreateReportActivity : AppCompatActivity() {
         val backButton = findViewById<Button>(R.id.back_button)
 
         keepButton.setOnClickListener {
+            SaveData()
             finish()
         }
 
         backButton.setOnClickListener {
             finish()
         }
+    }
+
+    //DBにレポートの内容を保存する(ネストが深くなりそうだったので関数にする)
+    private fun SaveData(){
+
     }
 }
