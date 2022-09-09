74c06a59485a29cb944219437c0325d27774f35c
Mitsuki4123 <mitsukikosen7@gmail.com>
2022-09-09 22:14:56 +0900

    GETに対応

--- app/src/main/java/com/example/procon33_remotetravelers_app/services/CheckTravelingService.kt
+++ app/src/main/java/com/example/procon33_remotetravelers_app/services/CheckTravelingService.kt
@@ -2,14 +2,11 @@ package com.example.procon33_remotetravelers_app.services
 
 import com.example.procon33_remotetravelers_app.models.apis.CheckTravelingResponse
 import retrofit2.Call
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.POST
+import retrofit2.http.*
 
 sealed interface CheckTravelingService {
-    @FormUrlEncoded
-    @POST("/api/viewer/check-travel")
+    @GET("/api/viewer/check-travel")
     fun checkTraveling(
-        @Field("user_id")user_id: Int,
+        @Query("user_id")user_id: Int,
     ): Call<CheckTravelingResponse>
 }
