09a7a38fa3f3520c353c2e9f4e853620e9a8c56d
Mitsuki4123 <mitsukikosen7@gmail.com>
2022-09-25 00:47:41 +0900

    透明ボタンの配置とレポートの表示_未完成

--- app/src/main/java/com/example/procon33_remotetravelers_app/activities/ViewAlbumActivity.kt
+++ app/src/main/java/com/example/procon33_remotetravelers_app/activities/ViewAlbumActivity.kt
@@ -1,25 +1,22 @@
 package com.example.procon33_remotetravelers_app.activities
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Base64
 import android.util.Log
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import com.example.procon33_remotetravelers_app.BuildConfig
 import com.example.procon33_remotetravelers_app.R
+import com.example.procon33_remotetravelers_app.models.apis.GetReports
 import com.example.procon33_remotetravelers_app.services.GetReportAllService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import java.nio.ByteBuffer
-import java.sql.Timestamp
-import java.util.Base64.getDecoder
 import kotlin.concurrent.thread
 
 class ViewAlbumActivity : AppCompatActivity() {
@@ -56,9 +53,8 @@ class ViewAlbumActivity : AppCompatActivity() {
                     Log.d("GetReportAllResponse", getReportAllResponse.toString())
                 }
 
-                for(i in getReportAllResponse.album){
-                    setReport(i.image, i.comment, i.created_at)
-                }
+                setReport(getReportAllResponse.album)
+
             } catch (e: Exception) {
                 Handler(Looper.getMainLooper()).post {
                     // エラー内容を出力
@@ -68,36 +64,85 @@ class ViewAlbumActivity : AppCompatActivity() {
         }
     }
 
-    private fun setReport(image: String, comment: String, created_at: String){
-        try {
-            val WC = LinearLayout.LayoutParams.WRAP_CONTENT
-            val MP = LinearLayout.LayoutParams.MATCH_PARENT
+    private fun setReport(reports: List<GetReports>){
+        //別スレッドでUIをいじるための回避策
+        runOnUiThread {
+            try {
+                val WC = LinearLayout.LayoutParams.WRAP_CONTENT
+                val MP = LinearLayout.LayoutParams.MATCH_PARENT
 
-            val albumFrame = findViewById<LinearLayout>(R.id.album_list)
-            val reportFrame = findViewById<LinearLayout>(R.id.report_liner)
+                val albumLiner = findViewById<LinearLayout>(R.id.album_list)
+                val reportLiner = findViewById<LinearLayout>(R.id.report_liner)
+                val albumFrame = findViewById<FrameLayout>(R.id.report_album_frame)
 
-            albumFrame.removeAllViews()
-            reportFrame.removeAllViews()
+                albumLiner.removeAllViews()
+                reportLiner.removeAllViews()
+                albumFrame.removeAllViews()
 
-            val decode = Base64.decode(image, Base64.DEFAULT)
-            val bitmap = Bitmap.createBitmap(16, 9, Bitmap.Config.ARGB_8888)
-            bitmap.copyPixelsFromBuffer(ByteBuffer.wrap(decode))
+                for (report in reports){
+                    val decodedBytes = Base64.decode(
+                        report.image.substring(report.image.indexOf(",") + 1),
+                        Base64.DEFAULT
+                    )
+                    val bitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
 
-            val image = ImageView(this)
-            image.setImageBitmap(bitmap)
-            reportFrame.addView(image, LinearLayout.LayoutParams(MP, WC))
+                    reportLiner.removeAllViews()
 
-            val text = TextView(this)
-            text.setText(created_at + comment)
-            reportFrame.addView(text, LinearLayout.LayoutParams(MP, WC))
+                    reportLiner.addView(
+                        setImageView(bitmap),
+                        LinearLayout.LayoutParams(WC, MP)
+                    )
 
-            albumFrame.addView(image, LinearLayout.LayoutParams(MP, WC))
-            albumFrame.addView(text, LinearLayout.LayoutParams(MP, WC))
-        }catch (e: Exception){
-            Handler(Looper.getMainLooper()).post {
-                // エラー内容を出力
-                Log.e("getCommentError", e.message.toString())
+                    val date = report.created_at.replace("-", "/").replace("T", " ").replace(".000000Z", "")
+
+                    reportLiner.addView(
+                        setTextView(date + "\n" + report.comment),
+                        LinearLayout.LayoutParams(WC, MP)
+                    )
+
+                    albumFrame.addView(
+                        reportLiner,
+                        LinearLayout.LayoutParams(MP, WC)
+                    )
+
+                    albumFrame.addView(
+                          setButton(),
+                        LinearLayout.LayoutParams(MP, MP)
+                    )
+
+                    albumLiner.addView(
+                        albumFrame,
+                        LinearLayout.LayoutParams(MP, WC)
+                    )
+                }
+
+            } catch (e: Exception) {
+                Handler(Looper.getMainLooper()).post {
+                    // エラー内容を出力
+                    Log.e("setReport", e.message.toString())
+                }
             }
         }
+    }
+
+    private fun setTextView (commentText: String): TextView{
+        val comment = TextView(this)
+        comment.text = commentText
+        comment.textSize = 20f
+        comment.setPadding(10, 15, 10, 15)
+        return comment
+    }
+
+    private fun setImageView(bitmap: Bitmap): ImageView{
+        val image = ImageView(this)
+        image.setImageBitmap(bitmap)
+        return image
+    }
+
+    private fun setButton(): Button{
+        val button = Button(this)
+        //遷移できるように透明のボタンにする
+        button.background = getDrawable(R.color.fide_report_button)
+        return button
     }
 }
