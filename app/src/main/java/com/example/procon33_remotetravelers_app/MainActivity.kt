0171193d3e96cf08cea2a3e3ca14c1fb4ddaada8
Mitsuki4123 <mitsukikosen7@gmail.com>
2022-10-07 10:10:06 +0900

    checkTravelの定期実行化

--- app/src/main/java/com/example/procon33_remotetravelers_app/MainActivity.kt
+++ app/src/main/java/com/example/procon33_remotetravelers_app/MainActivity.kt
@@ -19,8 +19,11 @@ import com.example.procon33_remotetravelers_app.services.SignupService
 import com.example.procon33_remotetravelers_app.services.StartTravelService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.reactivex.internal.subscriptions.SubscriptionHelper.cancel
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +37,8 @@ class MainActivity : AppCompatActivity() {
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
+    private val timer = Timer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -44,8 +49,10 @@ class MainActivity : AppCompatActivity() {
         if (userId.isNotEmpty()) {
             userId = getString(R.string.user_id_text, userId)
             userIdText.text = userId
-            //すでに旅行に参加しているかどうかの判断
-            checkTraveling()
+            timer.scheduleAtFixedRate(0, 5000){
+                //すでに旅行に参加しているかどうかの判断
+                checkTraveling()
+            }
         } else {
             signup(userIdText)
         }
@@ -53,6 +60,8 @@ class MainActivity : AppCompatActivity() {
         // 旅行するボタンが押されるとTravelerActivityに遷移する
         val travelButton = findViewById<Button>(R.id.travel_button)
         travelButton.setOnClickListener {
+            //自動参加と重複しないようにtimerをキャンセル
+            timer.cancel()
             startTravel()
             val intent = Intent(this, TravelerActivity::class.java)
             intent.putExtra("userId", getUserId().toInt())
@@ -158,44 +167,43 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkTraveling(){
         val userId = getUserId().toInt()
-
         //旅行をしているか、または旅行に参加しているかを判断するAPIを叩く
-        thread {
-            try {
-                val service: CheckTravelingService =
-                    retrofit.create(CheckTravelingService::class.java)
-                val checkTraveling = service.checkTraveling(
-                    user_id = userId
-                ).execute().body()
-                    ?: throw IllegalStateException("body is null")
+        try {
+            val service: CheckTravelingService =
+                retrofit.create(CheckTravelingService::class.java)
+            val checkTraveling = service.checkTraveling(
+                user_id = userId
+            ).execute().body()
+                ?: throw IllegalStateException("body is null")
 
-                Handler(Looper.getMainLooper()).post {
-                    // 実行結果を出力
-                    Log.d("checkTravellingResponse", checkTraveling.toString())
-                }
+            Handler(Looper.getMainLooper()).post {
+                // 実行結果を出力
+                Log.d("checkTravellingResponse", checkTraveling.toString())
+            }
 
-                //旅行者ならTravelerActivityに遷移
-                if(checkTraveling.traveling == true){
-                    if(checkTraveling.traveler == true){
-                        val intent = Intent(this, TravelerActivity::class.java)
-                        intent.putExtra("userId", getUserId().toInt())
-                        startActivity(intent)
-                    }else {
-                        val intent = Intent(this, ViewerActivity::class.java)
-                        intent.putExtra("userId", getUserId().toInt())
-                        startActivity(intent)
-                    }
+            //旅行者ならTravelerActivityに遷移
+            if(checkTraveling.traveling == true){
+                timer.cancel()
+                if(checkTraveling.traveler == true){
+                    val intent = Intent(this, TravelerActivity::class.java)
+                    intent.putExtra("userId", getUserId().toInt())
+                    startActivity(intent)
+                    finish()
+                }else {
+                    val intent = Intent(this, ViewerActivity::class.java)
+                    intent.putExtra("userId", getUserId().toInt())
+                    startActivity(intent)
                 }
-            } catch (e: Exception) {
-                Handler(Looper.getMainLooper()).post {
-                    // エラー内容を出力
-                    Log.e("error", e.message.toString())
+            }
+        } catch (e: Exception) {
+            Handler(Looper.getMainLooper()).post {
+                // エラー内容を出力
+                Log.e("error", e.message.toString())
 
-                    // 通信に失敗したことを通知
-                    val toast =
-                        Toast.makeText(this, "通信に失敗しました", Toast.LENGTH_SHORT)
-                    toast.show()
-                }
+                // 通信に失敗したことを通知
+                val toast =
+                    Toast.makeText(this, "通信に失敗しました", Toast.LENGTH_SHORT)
+                toast.show()
             }
         }
     }
